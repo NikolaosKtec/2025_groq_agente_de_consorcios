66b494e947f031b22e896e2cb8dc9dfd17aeca7b
nikolaos ferreira daskalakis <nikolaosf1daskalakis@gmail.com>
2025-11-04 09:28:04 -0300

    changes

--- server/index.js
+++ server/index.js
@@ -1,35 +1,33 @@
-import express from "express"
+import express, { json } from "express"
 import ChatAgent from "./ChatAgent.js"
 const chat = new ChatAgent();
 const app = express();
 
 app.use(express.json());
 
- app.post("/api/talk",async (req, res) => {
-       
-       
+app.get("/", (req, res) => {
+       res.json({message:"server ok"})
+})
+
+app.post("/api/talk", async (req, res) => {
+
+
        try {
               const { text, sender } = req.body;
               const responce = await chat.processUserInput(text);
               res.json({
-              text: responce,
-              sender: "agent"
-              
-       });
-       
-       
+                     text: responce,
+                     sender: "agent"
+
+              });
+
+
        } catch (e) {
               throw e;
-              // return res.status(400).json(
-              //        error:"houve um erro interno"
-              // )
        }
-       
-      // res.send(process.env.GROQ_API_KEY)
-
-
 })
 
-// const port = process.env.PORT || 3000
-
-export default app;
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+       console.log(`Serve sarted on: ${port}`)
+})
